eefd121ae33a113d686cb4d6a37f6a77c046ec56
saisay93 <saisay@hotmail.com>
2025-06-25 21:57:04 +0200

    ✅ F01: SRC > APP > Create routes & router

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,36 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+ import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+
+const routes = [
+	{
+		path: "/",
+		element: <HomePage />,
+	},
+	{
+		path: "/about",
+		element: <AboutPage />,
+	},
+	{
+		path: "/articles",
+		element: <ArticlesListPage />,
+	},
+	{ 
+		path: "/articles/individual",
+		element: <ArticlePage />,
+	},
+	
+];
+
+const router = createBrowserRouter(routes);
 
 function App() {
-	return <HomePage />;
+	return (
+		<RouterProvider router={router} />
+	);
 }
 
 export default App;
- 
