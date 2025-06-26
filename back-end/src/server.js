67e69e49a93380ab823ea48ca405d984291f3b07
saisay93 <saisay@hotmail.com>
2025-06-26 12:17:14 +0200

    📦 B01: install nodemon

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -10,11 +10,11 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/api/articles/:name/upvote", (req, res) => {
+app.post('/api/articles/:name/upvote', (req, res) => {
   const article = articleInfo.find((article) => article.name === req.params.name);
   article.upvotes += 1;
   
-  res.send(`Success! The article ${req.params.name} now has ${article.upvotes} upvotes!`);
+  res.send(`Success!  The article ${req.params.name} now has ${article.upvotes} upvotes!`);
 });
 
 app.listen(8000, () => {
