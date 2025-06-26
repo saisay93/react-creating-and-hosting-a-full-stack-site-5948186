e860ae79dede5987273ca5112c132ea024848200
saisay93 <saisay@hotmail.com>
2025-06-26 06:22:14 +0200

    ✅ F02: SRC > App > Update routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,36 +1,40 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
- import AboutPage from "./pages/AboutPage";
+import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
 import ArticlesListPage from "./pages/ArticlesListPage";
+import Layout from "./Layout";
 
 const routes = [
 	{
 		path: "/",
-		element: <HomePage />,
+		element: <Layout />,
+		children: [
+			{
+				path: "/",
+				element: <HomePage />,
+			},
+			{
+				path: "/about",
+				element: <AboutPage />,
+			},
+			{
+				path: "/articles",
+				element: <ArticlesListPage />,
+			},
+			{
+				path: "/articles/individual",
+				element: <ArticlePage />,
+			},
+		],
 	},
-	{
-		path: "/about",
-		element: <AboutPage />,
-	},
-	{
-		path: "/articles",
-		element: <ArticlesListPage />,
-	},
-	{ 
-		path: "/articles/individual",
-		element: <ArticlePage />,
-	},
-	
 ];
 
 const router = createBrowserRouter(routes);
 
 function App() {
-	return (
-		<RouterProvider router={router} />
-	);
+	return <RouterProvider router={router} />;
 }
 
 export default App;
