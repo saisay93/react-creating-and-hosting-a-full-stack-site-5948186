f6ffb294b270488ccbe50da99ce97b99e4a280ad
saisay93 <saisay@hotmail.com>
2025-06-26 12:06:35 +0200

    ✅ B01: SRC > Update endpoints

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,17 +1,22 @@
-import express from 'express';
+import express from "express";
+
+const articleInfo = [
+  { name: "learn-node", upvotes: 0 },
+  { name: "learn-react", upvotes: 0 },
+  { name: "mongodb", upvotes: 0 },
+];
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/hello', (req, res) => {
-	res.send("Hello! This is the back-end server.");
-})
-
-app.post("/submit", (req, res) => {
-	res.send(`Hello! ${req.body.name}, your form has been submitted.`);
-})
+app.post("/api/articles/:name/upvote", (req, res) => {
+  const article = articleInfo.find((article) => article.name === req.params.name);
+  article.upvotes += 1;
+  
+  res.send(`Success! The article ${req.params.name} now has ${article.upvotes} upvotes!`);
+});
 
 app.listen(8000, () => {
-  console.log("Server is listening on port 8000");
+	console.log("Server is listening on port 8000");
 });
