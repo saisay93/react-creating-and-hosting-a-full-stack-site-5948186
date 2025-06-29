0009317b44b73b93a4677a12a1959814a2d284a3
saisay93 <saisay@hotmail.com>
2025-06-29 20:10:59 +0200

    ✅ B01: SRC > Rewrite endpoints

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,36 +1,68 @@
 import express from "express";
-
-const articleInfo = [
-  { name: "learn-node", upvotes: 0, comments:[] },
-  { name: "learn-react", upvotes: 0, comments:[] },
-  { name: "mongodb", upvotes: 0, comments:[] },
-];
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/api/articles/:name/upvote', (req, res) => {
-  const article = articleInfo.find((article) => article.name === req.params.name);
-  article.upvotes += 1;
-  
-  res.json(article);
+let db;
+
+async function connectToDb() {
+	const uri = "mongodb://127.0.0.1:27017";
+	const client = new MongoClient(uri, {
+		serverApi: {
+			version: ServerApiVersion.v1,
+			strict: true,
+			deprecationErrors: true,
+		},
+	});
+
+	await client.connect();
+
+	db = client.db("full-stack-act-db");
+}
+
+app.get("/api/articles/:name", async (req, res) => {
+	const { name } = req.params;
+	const articles = await db.collection("articles").findOne({ name });
+	res.json(articles);
 });
 
-app.post('/api/articles/:name/comments', (req, res) => {
-  const {name} = req.params;
-  const {postedBy, text } = req.body;
+app.post("/api/articles/:name/upvote", async (req, res) => {
+	const { name } = req.params;
+	const updatedArticle = await db
+		.collection("articles")
+		.findOneAndUpdate(
+			{ name },
+			{ $inc: { upvotes: 1 } },
+			{ returnDocument: "after" },
+		);
 
-  const article = articleInfo.find((article) => article.name === name);
-  
-  article.comments.push({
-    postedBy,
-    text,
-  });
-
-  res.json(article);
+	return res.json(updatedArticle);
 });
 
-app.listen(8000, () => {
-	console.log("Server is listening on port 8000");
+app.post("/api/articles/:name/comments", async (req, res) => {
+	const { name } = req.params;
+	const { postedBy, text } = req.body;
+	const newComment = { postedBy, text };
+
+	const updatedArticle = await db
+		.collection("articles")
+		.findOneAndUpdate(
+			{ name },
+			{ $push: { comments: newComment } },
+			{ returnDocument: "after" },
+		);
+
+	return res.json(updatedArticle);
 });
+
+async function start() {
+	await connectToDb();
+
+	app.listen(8000, () => {
+		console.log("Server is listening on port 8000");
+	});
+}
+
+start();
